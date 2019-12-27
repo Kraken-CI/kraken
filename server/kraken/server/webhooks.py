28a79c4ec6c6bc853799e26041f6ba2b0702a58d
Michal Nowikowski <godfryd@gmail.com>
2019-12-27 07:23:23 +0100

    fixed webhook

--- server/kraken/server/webhooks.py
+++ server/kraken/server/webhooks.py
@@ -11,17 +11,13 @@ log = logging.getLogger(__name__)
 
 
 def handle_github_webhook(stage_id):
-    req = request.get_json()
-    log.info('GITHUB for stage_id:%s, payload: %s', stage_id, req)
+    payload = request.get_data()
+    log.info('GITHUB for stage_id:%s, payload: %s', stage_id, payload)
     event = request.headers.get('X-GitHub-Event')
     if event is None:
         log.warn('missing github event type in request header')
         abort(400, "missing github event type in request header")
     log.info('EVENT %s', event)
-    #if event == 'push':
-    repo_url = req['repository']['clone_url']
-    ref = req['ref']
-    branch = ref.split('/')[-1]
 
     # check stage
     stage = Stage.query.filter_by(id=stage_id).one_or_none()
@@ -37,17 +33,25 @@ def handle_github_webhook(stage_id):
     my_secret = None
     if stage.webhooks:
         my_secret = stage.webhooks.get('github_secret', None)
+        if my_secret:
+            my_secret = bytes(my_secret, 'ascii')
     if my_secret is not None:
         github_sig = request.headers.get("X-Hub-Signature")
         if github_sig is None:
             log.warn('missing signature in request header')
             abort(400, "missing signature in request header")
         github_digest_parts = github_sig.split("=", 1)
-        my_digest = hmac.new(my_secret, request.data, hashlib.sha1).hexdigest()
+        my_digest = hmac.new(my_secret, payload, hashlib.sha1).hexdigest()
 
         if len(github_digest_parts) < 2 or github_digest_parts[0] != "sha1" or not hmac.compare_digest(github_digest_parts[1], my_digest):
-            log.warn('bad signature %s', github_sig)
+            log.warn('bad signature %s vs %s, secret %s', github_sig, my_digest, my_secret)
             abort(400, "Invalid signature")
+
+    req = request.get_json()
+    #if event == 'push':
+    repo_url = req['repository']['clone_url']
+    ref = req['ref']
+    branch = ref.split('/')[-1]
 
     # trigger running the stage via celery
     trigger_data = dict(trigger='github-' + event,
