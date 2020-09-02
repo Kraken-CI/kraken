8a6a9f3b908396718ffbee42a23edfa97971781c
Michal Nowikowski <godfryd@gmail.com>
2020-09-02 21:53:21 +0200

    removed hardcoded example results data

--- ui/src/app/branch-results/branch-results.component.ts
+++ ui/src/app/branch-results/branch-results.component.ts
@@ -21,7 +21,6 @@ export class BranchResultsComponent implements OnInit {
     branch: any
     kind: string
 
-    flows0: any[]
     flows: any[]
 
     totalFlows = 100
@@ -46,188 +45,6 @@ export class BranchResultsComponent implements OnInit {
         this.kind = this.route.snapshot.paramMap.get('kind')
         this.selectedStage = null
         this.stagesAvailable = [{ name: 'All' }]
-        this.flows0 = [
-            {
-                name: '278',
-                state: 'completed',
-                created: '2019-09-13 15:30 UTC',
-                runs: [
-                    {
-                        name: 'Tarball',
-                        state: 'completed',
-                        color: '#e6ffe6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                    {
-                        name: 'Package',
-                        state: 'completed',
-                        color: '#e6ffe6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                    {
-                        name: 'System Tests',
-                        state: 'completed',
-                        color: '#e6ffe6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                        jobs_total: 103,
-                        jobs_error: 78,
-                        jobs_rerun: 7,
-                        jobs_not_run: 2,
-                        tests_passed: 987,
-                        tests_total: 1033,
-                        issues_total: 12,
-                    },
-                    {
-                        name: 'Deploy',
-                        state: 'not-run',
-                        color: '#e6ffe6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                    {
-                        name: 'Deploy Prod',
-                        state: 'completed',
-                        color: '#e6ffe6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                    {
-                        name: 'Deploy Prod 2',
-                        state: 'completed',
-                        color: '#e6ffe6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                    {
-                        name: 'Deploy Prod 3',
-                        state: 'completed',
-                        color: '#e6ffe6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                    {
-                        name: 'Deploy Prod 4',
-                        state: 'completed',
-                        color: '#e6ffe6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                ],
-            },
-            {
-                name: '277',
-                created: '2019-09-13 15:30 UTC',
-                state: 'completed',
-                runs: [
-                    {
-                        name: 'Tarball',
-                        state: 'completed',
-                        color: '#e6ffe6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                    {
-                        name: 'Package',
-                        state: 'completed',
-                        color: '#ffe6e6',
-                        jobs_total: 18,
-                        jobs_error: 3,
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                    {
-                        name: 'Deploy',
-                        color: '#ffffff',
-                        state: 'not-run',
-                        started: 'not run',
-                        duration: '---',
-                    },
-                ],
-            },
-            {
-                name: '276',
-                created: '2019-09-13 15:30 UTC',
-                state: 'completed',
-                runs: [
-                    {
-                        name: 'Tarball',
-                        state: 'completed',
-                        color: '#e6ffe6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                    {
-                        name: 'Package',
-                        color: '#e6ffe6',
-                        state: 'completed',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                    {
-                        name: 'System Tests',
-                        state: 'completed',
-                        color: '#fff9e6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                        tests_passed: 987,
-                        tests_total: 1033,
-                    },
-                    {
-                        name: 'Deploy',
-                        state: 'not-run',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                ],
-            },
-            {
-                name: '275',
-                created: '2019-09-13 15:30 UTC',
-                state: 'in-progress',
-                runs: [
-                    {
-                        name: 'Tarball',
-                        state: 'completed',
-                        color: '#e6ffe6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                    {
-                        name: 'Package',
-                        state: 'completed',
-                        color: '#e6ffe6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                    {
-                        name: 'System Tests',
-                        state: 'completed',
-                        color: '#fff9e6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                        tests_passed: 987,
-                        tests_total: 1033,
-                    },
-                    {
-                        name: 'Static Analysis',
-                        state: 'not-run',
-                        color: '#fff9e6',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                        issues_total: 78,
-                    },
-                    {
-                        name: 'Deploy',
-                        state: 'not-run',
-                        started: '2019-09-13 15:30 UTC',
-                        duration: '3h 40m',
-                    },
-                ],
-            },
-        ]
 
         this.executionService.getBranch(this.branchId).subscribe(branch => {
             this.titleService.setTitle('Kraken - Branch Results - ' + branch.name + ' ' + this.kind)
@@ -290,7 +107,6 @@ export class BranchResultsComponent implements OnInit {
                 const stages = new Set<string>()
                 this.totalFlows = data.total
                 flows = flows.concat(data.items)
-                flows = flows.concat(this.flows0)
                 for (const flow of flows) {
                     this._processFlowData(flow, stages)
                 }
