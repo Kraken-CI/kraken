012c668f9dd9868fcf0cad5ad2c21c1f3e2e7056
Michal Nowikowski <godfryd@gmail.com>
2020-10-12 08:56:48 +0200

    added initial stage schema for kraken

---
+++ .kraken/stage-1.py
@@ -0,0 +1,22 @@
+def stage(ctx):
+    return {
+        "parent": "root",
+        "triggers": {
+            "parent": True,
+        },
+        "flow_label": "hello-#{KK_FLOW_SEQ}",
+        "parameters": [],
+        "configs": [],
+        "jobs": [{
+            "name": "hello world",
+            "steps": [{
+                "tool": "shell",
+                "cmd": "echo 'hello world'"
+            }],
+            "environments": [{
+                "system": "any",
+                "agents_group": "all",
+                "config": "default"
+            }]
+        }]
+    }
