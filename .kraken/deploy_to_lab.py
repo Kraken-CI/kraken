1581653ed9ee09827124f76a220edae6b2b05e82
Michal Nowikowski <godfryd@gmail.com>
2020-12-27 16:12:06 +0100

    added workflow for deploying kraken to lab

---
+++ .kraken/deploy_to_lab.py
@@ -0,0 +1,36 @@
+def stage(ctx):
+    return {
+        "parent": "Build",
+        "triggers": {
+            "manual": True,
+        },
+        "parameters": [],
+        "configs": [],
+        "jobs": [{
+            "name": "deploy to lab",
+            "steps": [{
+                "tool": "artifacts",
+                "action": "download",
+                "source": "kraken.tar.gz"
+            }, {
+                "tool": "shell",
+                "cmd": "tar -zxf kraken.tar.gz",
+            }, {
+                "tool": "artifacts",
+                "action": "download",
+                "public": True,
+                "source": "kraken-docker-compose-0.#{KK_FLOW_SEQ}.yaml",
+                "cwd": "kraken"
+            }, {
+                "tool": "shell",
+                "cmd": "rake kk_ver=0.#{KK_FLOW_SEQ} deploy_lab",
+                "cwd": "kraken"
+            }],
+            "environments": [{
+                "system": "krakenci/bld-kraken",
+                "executor": "docker",
+            	"agents_group": "all",
+                "config": "default"
+            }]
+        }]
+    }
