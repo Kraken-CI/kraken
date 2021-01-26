814c748c26d301a1501115852faa9d0c0db6305e
Michal Nowikowski <godfryd@gmail.com>
2021-01-26 07:18:08 +0100

    by default autosplit is disabled

--- server/kraken/server/backend.py
+++ server/kraken/server/backend.py
@@ -430,7 +430,7 @@ def _handle_dispatch_tests(agent, req):
         # TODO
         raise NotImplementedError
 
-    if tests_cnt == 1:
+    if tests_cnt == 1 or 'autosplit' not in step or not step['autosplit']:
         _create_test_records(step, tests)
         db.session.commit()
         return {'tests': tests}
