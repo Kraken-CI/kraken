29eac15770594c8829de80308e1aeceab0f39c0d
Michal Nowikowski <godfryd@gmail.com>
2021-05-22 13:45:30 +0200

    added check in db for unique non-deleted agent addresses

---
+++ server/kraken/migrations/versions/9d8d8713c1ad_added_unique_index_for_address_of_non_.py
@@ -0,0 +1,30 @@
+"""added unique index for address of non-deleted agents
+
+Revision ID: 9d8d8713c1ad
+Revises: 19396769e240
+Create Date: 2021-05-22 07:04:18.171520
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = '9d8d8713c1ad'
+down_revision = '19396769e240'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # op.create_index('ix_agents_address_not_deleted',
+    #                 'agents',
+    #                 ['address'],
+    #                 unique=True,
+    #                 postgresql_where='deleted is NULL')
+    conn = op.get_bind()
+    conn.execute("CREATE UNIQUE INDEX ix_agents_address_not_deleted ON agents (address) WHERE deleted is NULL;")
+
+
+def downgrade():
+    op.drop_index('ix_agents_address_not_deleted')
