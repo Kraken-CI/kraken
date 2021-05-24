0b7f57c376d013d9967b5144342ea460702ffe2d
Michal Nowikowski <godfryd@gmail.com>
2021-05-24 22:49:06 +0200

    fixed lint issues

--- server/kraken/migrations/versions/9d8d8713c1ad_added_unique_index_for_address_of_non_.py
+++ server/kraken/migrations/versions/9d8d8713c1ad_added_unique_index_for_address_of_non_.py
@@ -6,8 +6,6 @@ Create Date: 2021-05-22 07:04:18.171520
 
 """
 from alembic import op
-import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = '9d8d8713c1ad'
@@ -17,11 +15,6 @@ depends_on = None
 
 
 def upgrade():
-    # op.create_index('ix_agents_address_not_deleted',
-    #                 'agents',
-    #                 ['address'],
-    #                 unique=True,
-    #                 postgresql_where='deleted is NULL')
     conn = op.get_bind()
     conn.execute("CREATE UNIQUE INDEX ix_agents_address_not_deleted ON agents (address) WHERE deleted is NULL;")
 
