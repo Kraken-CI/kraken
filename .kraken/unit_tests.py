f0197f1a1f48b7a1ca47733976927085c073ea86
Michal Nowikowski <godfryd@gmail.com>
2021-06-29 07:08:13 +0200

    server unit tests workflow: improvements

--- .kraken/unit_tests.py
+++ .kraken/unit_tests.py
@@ -45,6 +45,10 @@ def stage(ctx):
             "name": "pytest server",
             "steps": [{
                 "tool": "shell",
+                "cmd": "sudo apt-get update && sudo apt-get install -y --no-install-recommends apt-transport-https software-properties-common",
+                "timeout": 300
+            }, {
+                "tool": "shell",
                 "cmd": "bash -c \"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable' && sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-ce-cli\"",
                 "timeout": 300
             }, {
