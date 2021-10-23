d36c3699f5bc18a665b674d2f15ff935aa654fc2
Michal Nowikowski <godfryd@gmail.com>
2021-10-23 07:15:17 +0200

    unit_tests: increased job timeout

--- .kraken/unit_tests.py
+++ .kraken/unit_tests.py
@@ -46,7 +46,7 @@ def stage(ctx):
             }]
         }, {
             "name": "pytest server",
-            "timeout": 600,
+            "timeout": 1200,
             "steps": [{
                 "tool": "shell",
                 "cmd": "sudo apt-get update && sudo apt-get install -y --no-install-recommends apt-transport-https software-properties-common postgresql-client python3-pip python curl gpg-agent",
