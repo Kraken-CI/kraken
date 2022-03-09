84dd7a8e77a72b11510665f008b63943c5fe6117
Michal Nowikowski <godfryd@gmail.com>
2022-03-09 21:39:25 +0100

    WA for migration tests

--- server/kraken/server/models.py
+++ server/kraken/server/models.py
@@ -642,7 +642,11 @@ class TestCaseResult(db.Model):
     age = Column(Integer, default=0)
     change = Column(Integer, default=consts.TC_RESULT_CHANGE_NO)
     relevancy = Column(Integer, default=0)
-    comment_id = Column(Integer, ForeignKey('test_case_comments.id'), nullable=True)
+    comment_id = Column(Integer,
+                        ForeignKey('test_case_comments.id',
+                                   # this is WA, remember to set correct fk name in migration
+                                   name='fk_test_case_comments_test_case_results'),
+                        nullable=True)
     comment = relationship('TestCaseComment', back_populates="test_case_results")
 
     Index('ix_test_case_results_test_case_id', test_case_id)
