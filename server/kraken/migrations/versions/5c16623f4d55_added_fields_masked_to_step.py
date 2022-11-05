6b194c5c20fa75e7b232ee32992501de42db4d02
Michal Nowikowski <godfryd@gmail.com>
2022-11-05 18:51:24 +0100

    added fields_masked to step

---
+++ server/kraken/migrations/versions/5c16623f4d55_added_fields_masked_to_step.py
@@ -0,0 +1,24 @@
+"""added fields_masked to step
+
+Revision ID: 5c16623f4d55
+Revises: 9666bf8c1bb0
+Create Date: 2022-11-05 18:50:29.258670
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = '5c16623f4d55'
+down_revision = '9666bf8c1bb0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('steps', sa.Column('fields_masked', postgresql.JSONB(astext_type=sa.Text()), nullable=False))
+
+
+def downgrade():
+    op.drop_column('steps', 'fields_masked')
