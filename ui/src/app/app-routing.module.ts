0b0db53fdf80d5ddc15076a3276f6b0037b34938
Michal Nowikowski <godfryd@gmail.com>
2023-05-21 15:35:31 +0200

    updated angular from 14 to 15

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -128,7 +128,7 @@ const routes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+        RouterModule.forRoot(routes, {}),
     ],
     exports: [RouterModule],
 })
